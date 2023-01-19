a07390885b1e809130f89c96084a3db60c78b0d3
Your Name <jokarkuii@gmail.com>
2023-01-19 15:40:54 +0900

    Player,Main修正

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args){
     System.out.println("HERO!");
     Hero hero = new Hero();
-    System.out.println(hero.toString());
+    Player player = new Player();
+    
+    player.drawHeros();
+    player.showDeck();
     
     }
     
